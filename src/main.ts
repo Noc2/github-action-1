3e0a368b18f39f1eeefdccac67eadb47f41319db
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-12-12 15:35:26 +0100

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core'
+import { context } from '@actions/github'
+const io = require('@actions/io')
+import octokit from './octokit'
+import getCommitters from './graphql'
+import { getclas } from './checkcla'
+import { lockPullRequest } from './pullRequestLock'
+import { GitHub } from '@actions/github'
+
+
+
+async function run() {
+  try {
+    //const lock = core.getInput('lock')
+    const pullRequestNo: number = context.issue.number
+    core.debug('CLA Assistant GitHub Action has started')
+    core.debug('the PR No is ' + JSON.stringify(pullRequestNo))
+    if (context.payload.action === 'closed') {
+      return lockPullRequest(pullRequestNo)
+    }
+    else {
+      await getclas(pullRequestNo)
+      // const rateLimit = await octokit.rateLimit.get()
+      // console.log(JSON.stringify(rateLimit.data.resources, null, 4))
+    }
+
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+
+}
+run()
