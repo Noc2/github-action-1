ff02de22ccc90ad76c7a48fe090b8f63963e5274
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-12-18 15:34:36 +0100

    Development GitHub Actions

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { lockPullRequest } from './pullRequestLock'
 async function run() {
   try {
     const pullRequestNo: number = context.issue.number
-    console.log("the context is " + JSON.stringify(context))
+    console.log("the context is " + JSON.stringify(context, null, 2))
     core.info('CLA Assistant GitHub Action has started')
     core.info('the PR No is ' + JSON.stringify(pullRequestNo))
     if (context.payload.action === 'closed') {
