9ebf9303ae4cb9d7b9f62f473fe1ec3c49f65b18
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2019-12-18 15:26:31 +0100

    Development GitHub Actions

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,3 +1,9 @@
+import * as core from '@actions/core'
+import { GitHub } from '@actions/github'
+
+jest.mock('@actions/core')
+jest.mock('@actions/github')
+
 describe('TODO - Add a test suite', () => {
-  it('TODO - Add a test', async () => {});
+  it('TODO - Add a test', async () => { });
 });
